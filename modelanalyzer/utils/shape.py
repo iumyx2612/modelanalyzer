d438e2a84948e0c97c2380ad51be9d93b5f2c9a5
iumyx2612 <deftnottroanh@gmail.com>
2023-05-04 10:47:00 +0700

    Shape utils

---
+++ modelanalyzer/utils/shape.py
@@ -0,0 +1,41 @@
+from typing import Sequence
+from torch import Tensor
+
+
+__all__ = [
+    "nlc_to_nchw", "nchw_to_nlc"
+]
+
+
+# Scripts taken from OpenMMLab
+
+
+def nlc_to_nchw(x: Tensor,
+                hw_shape: Sequence[int]) -> Tensor:
+    """Convert [N, L, C] shape tensor to [N, C, H, W] shape tensor.
+
+    Args:
+        x (Tensor): The input tensor of shape [N, L, C] before conversion.
+        hw_shape (Sequence[int]): The height and width of output feature map.
+
+    Returns:
+        Tensor: The output tensor of shape [N, C, H, W] after conversion.
+    """
+    H, W = hw_shape
+    assert len(x.shape) == 3
+    B, L, C = x.shape
+    assert L == H * W, 'The seq_len doesn\'t match H, W'
+    return x.transpose(1, 2).reshape(B, C, H, W)
+
+
+def nchw_to_nlc(x: Tensor) -> Tensor:
+    """Flatten [N, C, H, W] shape tensor to [N, L, C] shape tensor.
+
+    Args:
+        x (Tensor): The input tensor of shape [N, C, H, W] before conversion.
+
+    Returns:
+        Tensor: The output tensor of shape [N, L, C] after conversion.
+    """
+    assert len(x.shape) == 4
+    return x.flatten(2).transpose(1, 2).contiguous()
